c0284f55ce12c15b58480d529f81b10d8f71023f
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-16 14:47:01 -0300

    Configuração do projeto e do git

---
+++ src/main/java/principal/HomeController.java
@@ -0,0 +1,16 @@
+package principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping()
+	public String homeApp(Model model) {
+		
+		model.addAttribute("mensagem", "Esta foi uma mensagem injetada  atraves do model");
+		return "index";
+	}
+}
